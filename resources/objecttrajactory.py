fe7d24ea85915f410c39402399195fc437314655
杨书轩 <bighead9851001@163.com>
2020-10-14 11:31:21 +0800

    增加支持搜索目标相似航迹

--- resources/objecttrajactory.py
+++ resources/objecttrajactory.py
@@ -2,10 +2,11 @@ from flask import request
 from database.postsqldb.models import ObjectTrajactoryModel
 from datetime import datetime
 from database.postsqldb.db import db
-
+from sqlalchemy.sql import select, func
 from flask_restful import Resource,  marshal_with
 from database.postsqldb.models import neighborhood_fields,neighborhood_area_fields
-
+from flask_restful import reqparse
+from sqlalchemy import desc,asc
 
 class ObjectTrajactoryApi(Resource):
   def put(self, id):
@@ -47,3 +48,17 @@ class ObjectTrajactorysApi(Resource):
     db.session.add(objectTrajactory)
     db.session.commit()
     return {'object_id': objectTrajactory.object_id}, 200
+
+class SimilarObjectTrajactorysApi(Resource):
+  from sqlalchemy import desc,asc
+  
+  def get(self,id):
+    parser = reqparse.RequestParser()
+
+    parser.add_argument('similar_num',type=int)
+
+    args = parser.parse_args()
+    similar_num = 10 if args['similar_num'] is None else args['similar_num']
+    objectTrajactory = ObjectTrajactoryModel.query.filter_by(object_id=id).first()
+    rows = ObjectTrajactoryModel.query.filter(ObjectTrajactoryModel.object_id!=id).order_by(asc(func.ST_HausdorffDistance(ObjectTrajactoryModel.gps_line,func.ST_AsEWKT(objectTrajactory.gps_line)))).limit(similar_num).all()
+    return  [row.dictRepr() for row in rows]
--- resources/routes.py
+++ resources/routes.py
@@ -3,7 +3,7 @@ from .car import CarsApi,CarApi
 from .neighborhood import NeighborhoodApi,NeighborhoodsApi,NeighborhoodAreaApi
 from .geometries3 import Geometries3sApi
 from .lastappeared import LastappearedApi,LastappearedsApi,LastappearedsFilterApi
-from .objecttrajactory import ObjectTrajactorysApi,ObjectTrajactoryApi,ObjectTrajactoryPredictorApi
+from .objecttrajactory import ObjectTrajactorysApi,ObjectTrajactoryApi,ObjectTrajactoryPredictorApi,SimilarObjectTrajactorysApi
 def initialize_routes(api):
  
  
@@ -24,3 +24,5 @@ def initialize_routes(api):
     api.add_resource(ObjectTrajactoryPredictorApi,'/objecttrajactorypredictor/<id>')
 
     api.add_resource(LastappearedsFilterApi,'/lastappearedfilter')
+
+    api.add_resource(SimilarObjectTrajactorysApi,'/similarobjecttrajactorysapi/<id>')
