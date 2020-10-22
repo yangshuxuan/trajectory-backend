dd292f96ba2e4cef064713ce0ab11c5f9c7bdcfd
杨书轩 <bighead9851001@163.com>
2020-10-22 15:42:10 +0800

    建立轨迹滤波的框架

--- resources/trajectorypreprocess/filter.py
+++ resources/trajectorypreprocess/filter.py
@@ -2,9 +2,11 @@ from database.postsqldb.trajectorypreprocess.trajectoryfiltermodel import Object
 from flask_restful import Resource
 from database.postsqldb.models import ObjectTrajactoryModel
 class ObjectTrajactoryFilterApi(Resource):
+
   def get(self,id):
-    p = ObjectTrajactoryAfterFilterModel.query.get(id)
-    if p is None:
-        p = ObjectTrajactoryModel.query.get(id)
+    """
+    
+    """
+    p = ObjectTrajactoryAfterFilterModel.trajectoryfilter(id)
 
     return p.dictRepr(),200
